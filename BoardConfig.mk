1ba6091a05a1d2c7063c4e2235bc5a7789c8df77
ShazuxD <shazuxd7@gmail.com>
2023-07-13 16:43:19 +0530

    fleur: drop not needed logical partitions
    Signed-off-by: ShazuxD <shazuxd7@gmail.com>
    Change-Id: Ib70bdba0d4fe6e8e09a3523af5b2c55d3ff770ca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,4 +161,3 @@ TW_EXCLUDE_APEX := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
-TW_INCLUDE_LOGICAL := my_product my_engineering my_preload
